e9142b84cd05a610ff861e4783495fb65621dd49
Benjamin Thaut <dasaxmansbrother@hotmail.com>
2020-03-07 14:32:06 -0700

    stuff

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
-app.set("views", "views");
-app.set("view engine", "ejs");
+// app.set("views", "views");
+// app.set("view engine", "ejs");
 
 app.get("/", function(req, res) { 
     console.log("Received a request for /");
@@ -14,14 +14,14 @@ app.get("/", function(req, res) {
     res.end();
 });
 
-app.get("/home", function(req, res) {
-    console.log("Received a request for the home page.");
+// app.get("/home", function(req, res) {
+//     console.log("Received a request for the home page.");
 
-    var name = "John";
-    var emailAddress = "john@email.com";
-    var params = {username: name, email: emailAddress};
-    res.render("home", params);
-});
+//     var name = "John";
+//     var emailAddress = "john@email.com";
+//     var params = {username: name, email: emailAddress};
+//     res.render("home", params);
+// });
 
 app.listen(port, function() {
     console.log("The server is up and listening on port 3000!");
